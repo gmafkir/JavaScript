6e3d45c888f06fc1669f8743e59d76279a3158d6
gmafkir <9020340@student.zadkine.nl>
2024-11-06 15:42:55 +0100

    opdracht 11

--- opdracht 10/app.js
+++ opdracht 10/app.js
@@ -1,19 +1,40 @@
 let answer = document.getElementById('answer');
 let num1 = document.getElementById('num1');
 let num2 = document.getElementById('num2');
-function multiply(){
-let ans = num1.value * num2.value;
-answer.innerText = ans;
-}
-function divide(){
-let ans = num1.value / num2.value;
-answer.innerText = ans;
-}
-function add(){
-let ans = num1.value + num2.value;
-answer.innerText = ans;
-}
-function minus(){
-let ans = num1.value - num2.value;
-answer.innerText = ans;
-}
+
+
+    function multiply() {
+        if(num1.value > 0 && num2.value>0){
+            let ans = num1.value * num2.value;
+            answer.innerText = ans;
+        } else {
+            answer.innerText = "kan niet door 0 berekenen"
+        }
+    }
+
+    function divide() {
+        if(num1.value > 0 && num2.value>0) {
+            let ans = num1.value / num2.value;
+            answer.innerText = ans;
+        } else {
+            answer.innerText = "kan niet door 0 berekenen";
+        }
+    }
+
+    function add() {
+        if(num1.value > 0 && num2.value>0) {
+            let ans = num1.value + num2.value;
+            answer.innerText = ans;
+        } else {
+            answer.innerText = "kan niet door 0 berekenen";
+        }
+    }
+
+    function minus() {
+        if(num1.value > 0 && num2.value>0) {
+            let ans = num1.value - num2.value;
+            answer.innerText = ans;
+        } else {
+            answer.innerText = "kan niet door 0 berekenen";
+        }
+    }
